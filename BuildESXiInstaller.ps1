49301d2f1f2a1f839a7b21a881e1455aa09d9c52
Albert Hui <albert.hui@gmail.com>
2021-03-18 04:15:17 +0800

    Add files via upload

---
+++ BuildESXiInstaller.ps1
@@ -0,0 +1,36 @@
+# Install VMware PowerCLI module
+Install-Module -Name VMware.PowerCLI -Scope CurrentUser -AllowClobber
+
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false
+
+# Build profile names
+$baseProfile = "ESXi-7.0.0-15843807-standard"
+$newProfile = "ESXi-7.0.0-15843807-NUC"
+
+# Clear existing ESX software depots
+Get-EsxSoftwareDepot | Remove-EsxSoftwareDepot
+
+# Download ESX base profile software depot
+Add-EsxSoftwareDepot https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
+Export-ESXImageProfile -ImageProfile $baseProfile -ExportToBundle -filepath ($baseProfile + ".zip")
+Remove-EsxSoftwareDepot https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
+Add-EsxSoftwareDepot (".\" + $baseProfile + ".zip")
+
+# Add drivers as ESX software depots
+Add-EsxSoftwareDepot .\ESXi670-NE1000-32543355-offline_bundle-15486963.zip
+Add-EsxSoftwareDepot .\ESXi700-VMKUSB-NIC-FLING-39035884-component-16770668.zip
+
+# Create new NUC profile
+Remove-EsxImageProfile -ImageProfile $newProfile
+New-EsxImageProfile -CloneProfile $baseProfile -name $newProfile -Vendor "virten.net"
+
+# Inject new ne1000 NIC driver
+Remove-EsxSoftwarePackage -ImageProfile $newProfile -SoftwarePackage "ne1000"
+Add-EsxSoftwarePackage -ImageProfile $newProfile -SoftwarePackage "ne1000 0.8.4-3vmw.670.3.99.32543355"
+
+# Inject USB NIC Fling driver
+Add-EsxSoftwarePackage -ImageProfile $newProfile -SoftwarePackage "vmkusb-nic-fling"
+
+# Export ESXi install ISO and bundle
+Export-ESXImageProfile -ImageProfile $newProfile -ExportToISO -filepath (".\" + $newProfile + ".iso")
+Export-ESXImageProfile -ImageProfile $newProfile -ExportToBundle -filepath (".\" + $newProfile + ".zip")
